3fc0789020936d8d6a5e82e7de31645e92bc5845
Himakar <54240267+Himakar098@users.noreply.github.com>
2021-09-04 12:04:52 +0530

    Add files via upload

---
+++ Final Check/moviecruiser/src/main/java/com/cognizant/moviecruiser/MoviecruiserApplication.java
@@ -0,0 +1,14 @@
+package com.cognizant.moviecruiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecruiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecruiserApplication.class, args);
+		System.out.println("in main");
+	}
+
+}
